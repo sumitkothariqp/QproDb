3e6ceb9df34a2feb3ce143f241d986251e289571
rekhajadhav <rekha.jadhav@questionpro.com>
2019-03-25 15:53:25 +0530

    my work

--- db/src/qpro/command/HelpCommand.java
+++ db/src/qpro/command/HelpCommand.java
@@ -1,6 +1,9 @@
-package qpro.command;
+package com.qpro.command;
 
-public class HelpCommand  implements CommandInterface{
+public class HelpCommand implements CommandInterface {
+
+
+
 
     @Override
     public boolean isValidate(String query) {
@@ -11,17 +14,18 @@ public class HelpCommand  implements CommandInterface{
     }
 
     @Override
-    public Object process(String query) {
+    public void process (String query) {
+
         if(query.contains("help")){
-            System.out.println("Commands:\n\t\t create \n\t\t select");
+            System.out.println("Commands: \n\t\t create;\n\t\t select;");
             System.out.println("check for help type: 'help create' \n 'help select'");
-        }
-        else if(query.contains("help create")){
-            System.out.println("create command: 'create table tablename (column_name column_type");
+        }else if(query.contains("help create")){
+            System.out.println("create command: create table tablename (column_name column_type)");
         }
         else if(query.contains("help select")){
-            System.out.println("select command: 'select */column_name from tablename");
+            System.out.println("select command: select <*/column_name> from tablename ");
         }
-        return null;
+
+
     }
 }
