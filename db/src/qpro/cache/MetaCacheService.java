18537ccbdffb0318d581c60c98456919bf8515ed
sumit <sumit.kothari@questionpro.com>
2019-03-25 16:31:26 +0530

    changes

--- db/src/qpro/cache/MetaCacheService.java
+++ db/src/qpro/cache/MetaCacheService.java
@@ -1,37 +1,36 @@
 package qpro.cache;
 
-import file.util.FileObjectReader;
 import qpro.meta.TableMeta;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 public class MetaCacheService {
 
 
-    private static Map<String, TableMeta> metaCache = Cache.getINSTANCE().getMetaCache();
+    private static  Map<String, TableMeta> metaCache = Cache.getINSTANCE().getMetaCache();
 
-    public boolean isTableExits(String name) {
+    public static boolean isTableExits(String name) {
         if (metaCache.containsKey(name)) {
             return false;
         }
         return true;
     }
 
-    public TableMeta getTableMeta(String name) {
+    public static TableMeta getTableMeta(String name) {
         return metaCache.get(name);
     }
 
-    public void createTable(TableMeta tableMeta) {
+    public static Collection<TableMeta> getAllTables() {
+        return metaCache.values();
+    }
+
+    public static void createTable(TableMeta tableMeta) {
         metaCache.put(tableMeta.getName(), tableMeta);
     }
 
-    public static void initializeCache() {
-        FileObjectReader fileObjectReader = new FileObjectReader();
-        List<Object> fileObject = fileObjectReader.readObjectsFromFile("metadata.dat");
-        List<TableMeta> tableMetaList = (List<TableMeta>) fileObject.get(0);
-        if (tableMetaList != null) {
-            tableMetaList.stream().forEach(tableMeta -> metaCache.put(tableMeta.getName(), tableMeta));
-        }
+    public static void initializeCache(List<TableMeta> tableMetaList) {
+        tableMetaList.stream().forEach(tableMeta -> metaCache.put(tableMeta.getName(), tableMeta));
     }
 }
